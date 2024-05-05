565c234d8d99f894ff147396223c836c0e263738
kANNISTER <77148257+kANNISTER@users.noreply.github.com>
2024-05-05 01:46:42 -0700

    Prediction working

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
 from flask import Flask, render_template
+import joblib
 import requests
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from collections import Counter
 import pickle
+import json
+import numpy as np
 
 app = Flask(__name__)
-
-loaded_model = None
-
-# Load the model from the pickle file
-with open("data/cricketPrediction.pkl", "rb") as f:
-    loaded_model = pickle.load(f)
-    
 
 import requests
 import pandas as pd
 
-mainUrl = "https://garbage-cheat-reads-protect.trycloudflare.com"
+loaded_model = None
+
+with open("data/cricketPrediction.pkl", "rb") as f:
+    loaded_model = pickle.load(f)
+
+
+
+mainUrl = "http://127.0.0.1:8100"
 
 getEntireLiveDataApi = "/analyzingtrends/getentirelivedata" 
 getCurrentLiveDataApi = "/analyzingtrends/getcurrentlivedata"
+
+
 
 def getAllLiveData():
   response = requests.get(mainUrl + getEntireLiveDataApi)
@@ -53,7 +59,7 @@ def getCurrentLiveData():
 def processLiveFeed():
 
     liveData0, liveJson = getCurrentLiveData()
-
+    liveData0 = getAllLiveData()
 
     new_columns = ['match_id', 'season', 'venue', 'innings', 'ball', 'batting_team',
                 'bowling_team', 'striker', 'non_striker', 'bowler', 'runs_off_bat',
@@ -63,47 +69,61 @@ def processLiveFeed():
 
     # Rename columns
     liveData0.columns = new_columns
-    liveData = liveData0.filter(['batting_team', 'bowling_team', 'non_striker', 'bowler', 'team1',
-        'team2', 'toss_winner', 'ball', 'runs_off_bat', 'dl_applied'])
-
-    to_norm = (liveData.select_dtypes(include =['int64', 'float64'])) #select continuous variables
-
-    continuous = MinMaxScaler().fit_transform(to_norm) #fit and transform min max scaler (normalizes)
-    continuous = pd.DataFrame(continuous, index = liveData.index, columns = list(to_norm))
-
-    #encode categorical variables
-
-    to_encode = (liveData.select_dtypes(include =['object'])) #test if anything is an object or category variable
-
-    label_encoders = [] #make new encoder for each column
-    encoded_data = pd.DataFrame()
-
-    encoding_dicts = {}
-
-    # Iterate over each column in 'to_encode' and encode using a separate LabelEncoder
-    for col in to_encode:
-        # Create a new instance of LabelEncoder for the current column
-        encoder = LabelEncoder()
-        
-        # Fit and transform the data in 'from02to22[col]'
-        encoded_data[col] = encoder.fit_transform(liveData[col])
-        
-        # Store the encoder in the list
-        label_encoders.append(encoder)
-
-        encoding_dicts[col] = dict(zip(encoder.classes_, encoder.transform(encoder.classes_)))
+    liveData = liveData0.filter(['innings', 'ball', 'batting_team', 'bowling_team', 'striker',
+       'non_striker', 'bowler', 'runs_off_bat', 'extras', 'team1', 'team2',
+       'toss_winner', 'toss_decision', 'dl_applied'])
 
 
-    #patch the columns back together
+    liveData['toss_winner'] = liveData.apply(lambda row: 'team1' if row['toss_winner'] == row['team1'] else 'team2', axis=1)
 
-    data = pd.concat([encoded_data.reset_index(drop=True), continuous.reset_index(drop=True)], axis = 1) #reset indices to avoid errors in concat
 
-    predictRFC = loaded_model.predict(data)
+    # Load the team map dictionary from the saved JSON file
+    with open('data/team_map.json', 'r') as f:
+        team_map_loaded = json.load(f)
+
+    with open('data/player_map.json', 'r') as f:
+        player_map_loaded = json.load(f)
+
+    team_map_loaded = dict(team_map_loaded)
+    player_map_loaded = dict(player_map_loaded)
+
+    liveData['team1'] = liveData['team1'].apply(lambda x: team_map_loaded.get(x))
+
+    liveData['team2'] = liveData['team2'].apply(lambda x: team_map_loaded.get(x))
+
+    liveData['batting_team'] = liveData['batting_team'].apply(lambda x: team_map_loaded.get(x))
+
+    liveData['bowling_team'] = liveData['bowling_team'].apply(lambda x: team_map_loaded.get(x))
+
+    liveData['striker'] = liveData['striker'].apply(lambda x: player_map_loaded.get(x))
+
+    liveData['non_striker'] = liveData['non_striker'].apply(lambda x: player_map_loaded.get(x))
+
+    liveData['bowler'] = liveData['bowler'].apply(lambda x: player_map_loaded.get(x))
+
+    liveData['toss_winner'] = liveData['toss_winner'].apply(lambda x: 0 if x == "team1" else 1)
+
+    liveData['toss_decision'] = liveData['toss_decision'].apply(lambda x: 0 if x == "bat" else 1)
+
+    loaded_min_max_scaler = joblib.load('data/min_max_scaler.joblib')
+
+    liveData[['innings', 'ball', 'runs_off_bat', 'extras', 'dl_applied']] = loaded_min_max_scaler.transform(liveData[['innings', 'ball', 'runs_off_bat', 'extras', 'dl_applied']])
+
+
+    predictRFC = loaded_model.predict(liveData)
 
     # Apply mapping function directly to the predicted values (assuming predictRFC is a Series or array)
+    print(predictRFC)
     out = pd.Series(predictRFC).apply(map_team)
     out = pd.DataFrame(out)
     out['match_id'] = liveData0['match_id']
+    print(liveData.head())
+    
+    # countTeamPrediction = Counter(out[0])
+
+    # print(countTeamPrediction)
+
+    return out, liveJson, predictRFC
 
 def map_team(value):
     if value == 0:
@@ -112,13 +132,22 @@ def map_team(value):
         return 'team2'
 
 
-
-
-
 @app.route('/')
 def index():
-    liveData0, liveJson = getCurrentLiveData()
-    return render_template('index.html', match_data = liveJson)
+    out, liveJson, predictRFC = processLiveFeed()
+
+    liveData, t = getCurrentLiveData()
+
+
+    zeros = np.count_nonzero(predictRFC == 0)
+    total = len(predictRFC)
+
+    percTeam1 = (zeros/total) * 100
+    percTeam2 = 100 - percTeam1
+
+    print(liveData.get('team1')[0])
+
+    return render_template('index.html', match_data = liveJson, predictWinTeam1 = liveData.get('team1')[0] + " : " + str(round(percTeam1, 2)) + "%", predictWinTeam2 = liveData.get('team2')[0] + " : " + str(round(percTeam2, 2))  + "%")
 
 if __name__ == '__main__':
     app.run()
