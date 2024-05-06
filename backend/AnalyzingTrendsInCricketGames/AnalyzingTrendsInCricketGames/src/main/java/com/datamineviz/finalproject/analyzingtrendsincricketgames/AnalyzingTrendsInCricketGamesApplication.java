86475d2d03f05ac2148e7e95191def0f6ff2ef18
kANNISTER <77148257+kANNISTER@users.noreply.github.com>
2024-05-05 19:33:47 -0700

    Added backend main

---
+++ backend/AnalyzingTrendsInCricketGames/AnalyzingTrendsInCricketGames/src/main/java/com/datamineviz/finalproject/analyzingtrendsincricketgames/AnalyzingTrendsInCricketGamesApplication.java
@@ -0,0 +1,18 @@
+package com.datamineviz.finalproject.analyzingtrendsincricketgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class AnalyzingTrendsInCricketGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzingTrendsInCricketGamesApplication.class, args);
+	}
+	
+	
+ 
+}
